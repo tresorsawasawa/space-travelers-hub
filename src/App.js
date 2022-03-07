24bfd8db536b70d1c281f58aebe26eede36105d7
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-07 18:42:41 +0200

    Fix some linters errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-
 const App = () => (
-    <div className="App">
-      <h1>Hello Team</h1>
-    </div>
-  );
-
+  <div className="App">
+    <h1>Hello Team</h1>
+  </div>
+);
 
 export default App;
