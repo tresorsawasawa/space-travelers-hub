f21a8a160b0dc0175140416235cf83b45a1dd0d7
Kandy Peter <kandypeter03@gmail.com>
2022-03-08 16:23:16 +0200

    fetch data from mission api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import MyProfile from './components/MyProfile';
+import store from './redux/configureStore';
 
 const App = () => (
   <div className="App">
     <Header />
-    <Routes>
-      <Route path="/" element={<Rockets />} />
-      <Route path="/Missions" element={<Missions />} />
-      <Route path="/MyProfile" element={<MyProfile />} />
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/MyProfile" element={<MyProfile />} />
+      </Routes>
+    </Provider>
   </div>
 );
 
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import { render } from '@testing-library/react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getFromApi } from '../redux/missions/missions';
 
-const Missions = () => (
-  <div>
-    <h1>Missions Page</h1>
-  </div>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getFromApi());
+  }, [dispatch]);
+
+  render(
+    <div className="clm-row">
+      Mission fetched
+    </div>,
+  );
+};
 
 export default Missions;
