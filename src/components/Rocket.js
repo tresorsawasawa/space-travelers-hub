e9e5be29cad3e9a0a289595bba4b61159625591b
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-09 17:25:43 +0200

    Add some logics to make the 'BOOK_ROCKET' action functional

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { bookRocket } from '../redux/rockets/rockets';
 
 const Rocket = ({
-  id, description, name, image,
-}) => (
-  <div className="rocket-container flex-center-center">
-    <div className="image-container">
-      <img src={image} alt={name} />
+  id, description, name, image, reserved,
+}) => {
+  const dispatch = useDispatch();
+  const bookingRocket = (e) => {
+    const { id } = e.target;
+    dispatch(bookRocket(+id));
+  };
+
+  return (
+    <div className="rocket-container flex-center-center">
+      <div className="image-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="rocket-infos">
+        <h3 className="rocket-name">{name}</h3>
+        <p className="description">
+          {reserved && (<span className="isReserved">Reserved</span>)}
+          {description}
+        </p>
+        {!reserved && (
+          <button
+            type="button"
+            id={id}
+            className="reserve-btn clickable"
+            onClick={bookingRocket}
+          >
+            Reserve Rocket
+          </button>
+        )}
+      </div>
     </div>
-    <div className="rocket-infos">
-      <h3 className="rocket-name">{name}</h3>
-      <p className="description">{description}</p>
-      <button type="button" id={id} className="reserve-btn">Reserve Rocket</button>
-    </div>
-  </div>
-);
+  );
+};
 
 Rocket.propTypes = {
   id: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default Rocket;
