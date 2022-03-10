6bcb14e23f0491d680d11e39b7724643c4f8e2eb
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-10 09:47:15 +0200

    Add rocket booking cancelation action

--- src/redux/rockets/rocketsActions.js
+++ src/redux/rockets/rocketsActions.js
@@ -1,4 +1,5 @@
 const GET_ROCKETS = 'space-travelers-hub/rockets/GET_ROCKETS';
 const BOOKING_ROCKET = 'space-travelers-hub/BOOKING_ROCKETS';
+const CANCEL_ROCKET_BOOKING = 'space-travelers-hub/CANCEL_ROCKET_BOOKING';
 
-export { GET_ROCKETS, BOOKING_ROCKET };
+export { GET_ROCKETS, BOOKING_ROCKET, CANCEL_ROCKET_BOOKING };
